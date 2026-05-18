e564a312e72cf2c5d45b53bdbd73a681b81bd5ba
Arihant Saini <arihantsaini@example.com>
2026-05-18 09:26:48 +0530

    Fix AdSense script attribute

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -63,7 +63,7 @@ export default function RootLayout({ children }) {
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9194178610009666"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
       </head>
       <body suppressHydrationWarning>
